32d0088c9e397ac21527f13ba2562bae2c292455
saschageyer <sascha.geyer@init.de>
2021-03-15 12:20:12 +0100

    refactored score

--- code/Model.py
+++ code/Model.py
@@ -11,27 +11,20 @@ class Model:
     
 
     def train_test_split(self, config):
-        """
-        split data into X_train, y_train, X_test, y_test
-        The size of X_train is determined by the train window (weeks)
-        We perform Weight of Evidence Encoding (WOE) for shoppers and products
-        We separating target (purchased) and features
-        """
+        '''
+        Split data according to model config (test_week and train_window)
+        '''
         test_week = config['model']['test_week']
         train_window = config['model']['train_window']
         data = self.data
         
-
-        # Split test_week and train_weeks
-        # ------------------------------------------------------------------------------
         start = test_week - train_window
         train = data[(data["week"] >= start) & (data["week"] < test_week)]
         test = data[data["week"] == test_week]
+        
         train, test = self.woe_encode(train, test, 'shopper')
         train, test = self.woe_encode(train, test, 'product')
         
-        # Split features X and target y
-        # ------------------------------------------------------------------------------
         non_features = ["shopper", "week", "product", "purchased"]
         features = [col for col in train.columns if col not in non_features]
 
@@ -44,6 +37,9 @@ class Model:
 
     
     def woe_encode(self, train, test, feature):
+        '''
+        Weight of Evidence Encoding (WOE)
+        '''
         train = train.copy()
         test = test.copy()
         encoder = category_encoders.WOEEncoder()
@@ -59,26 +55,23 @@ class Model:
                 
 
     def fit(self, X_train, y_train, **kwargs):
-        """
+        '''
         Fit a simple LGBM Classifier
-        """
+        '''
         lgbm_classifier = lightgbm.LGBMClassifier()
         lgbm_classifier.fit(X_train, y_train, **kwargs)
         self.lgbm_classifier = lgbm_classifier
 
     
     def predict(self, X_test):
-        """
+        '''
         Compute purchase probability predictions
-        """
+        '''
         lgbm_classifier = self.lgbm_classifier
         y_hat = lgbm_classifier.predict_proba(X_test)[:, 1]
         return y_hat
 
     
-    def get_score(self, y, y_hat):
-        """
-        Calculate log_loss score
-        """
+    def log_loss_score(self, y, y_hat):
         metric = sklearn.metrics.log_loss
         return metric(y, y_hat)
